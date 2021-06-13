46880c5ab368bcd4e8d6b1b67f42d3a1ab1d96f0
mujin-pro <greedy.hyena.boy@gmail.com>
2021-06-14 07:58:42 +0900

    Revert "ストロングパラーメーターの修正"
    This reverts commit e6263a46524c703c5fb07ba15248f48912df44bb.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :password_confirmation, :family_name, :first_name, :family_name_furigana, :first_name_furigana, :birth_day] )
-  end
 end
--- db/migrate/20210612214625_rename_last_name_column_to_users.rb
+++
@@ -1,6 +0,0 @@
-class RenameLastNameColumnToUsers < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :users, :last_name, :first_name
-    rename_column :users, :last_name_furigana, :first_name_furigana
-  end
-end
