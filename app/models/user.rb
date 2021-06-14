c83f1bcc008927e34270ae5b9acda670532b5662
mujin-pro <greedy.hyena.boy@gmail.com>
2021-06-15 08:58:35 +0900

    ユーザー新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_furigana, presence: true
+  validates :first_name_furigana, presence: true
+  validates :birth_day, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
