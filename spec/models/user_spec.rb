b2edf924d167b43a0a4cc7c3312e50390b686038
mujin-pro <greedy.hyena.boy@gmail.com>
2021-06-18 04:46:44 +0900

    Rspecのインストールと初期設定・テストコードひな形作成

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+    # nicknameが空では登録できないテストコードを記述する
+    end
+
+    it 'emailが空では登録できない' do
+    # emailが空では登録できないテストコードを記述する
+    end
+  end
+end
