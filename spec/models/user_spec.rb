afa378d8fbf760a2266b44ad3e07a2bef667b5ee
mujin-pro <greedy.hyena.boy@gmail.com>
2021-06-18 07:09:19 +0900

    Revert "テストコード作成"
    This reverts commit 7a14ad4abec44b9a619dd39908852df25829e9ba.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
-      user = User.new(nickname:'', email: 'test@example', password: '000000', password_confirmation: '000000', family_name: '山田', first_name: '太郎', family_name_furigana: 'ヤマダ', first_name_furigana: 'タロウ', birth_day: '2000/01/01')
-      user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    # nicknameが空では登録できないテストコードを記述する
     end
 
     it 'emailが空では登録できない' do
-      user = User.new(nickname:'ドカベン', email: '', password: '000000', password_confirmation: '000000', family_name: '山田', first_name: '太郎', family_name_furigana: 'ヤマダ', first_name_furigana: 'タロウ', birth_day: '2000/01/01')
-      user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
+    # emailが空では登録できないテストコードを記述する
     end
-
-    
   end
 end
