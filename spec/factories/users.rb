797d3da5115387d625712dae17f13cef55f8044a
mujin-pro <greedy.hyena.boy@gmail.com>
2021-06-19 14:29:50 +0900

    Fakerの導入・ユーザー新規登録のバリデーション機能テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname                {'test'}
-    email                   {'test@example'}
-    password                {'000000'}
-    password_confirmation   {'000000'}
-    family_name             {'山田'}
-    first_name              {'太郎'}
-    family_name_furigana    {'ヤマダ'}
-    first_name_furigana     {'タロウ'}
-    birth_day               {'2000/01/01'}
+    nickname                {Faker::Name.initials(number: 2)}
+    email                   {Faker::Internet.free_email}
+    password                {Faker::Internet.password(min_length: 6)}
+    password_confirmation   {password}
+    family_name             {Faker::Name.last_name}
+    first_name              {Faker::Name.first_name}
+    family_name_furigana    {Faker::Name.initials(number: 2)}
+    first_name_furigana     {Faker::Name.initials(number: 2)}
+    birth_day               {Faker::Date.birthday}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,5 +16,35 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
+
+    it 'family_nameが空では登録できない' do
+      @user.family_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Family name can't be blank")
+    end
+
+    it 'first_nameが空では登録できない' do
+      @user.first_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it 'family_name_furiganaが空では登録できない' do
+      @user.family_name_furigana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Family name furigana can't be blank")
+    end
+
+    it 'first_name_furiganaが空では登録できない' do
+      @user.first_name_furigana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name furigana can't be blank")
+    end
+
+    it 'birth_dayが空では登録できない' do
+      @user.birth_day = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birth day can't be blank")
+    end
   end
 end
