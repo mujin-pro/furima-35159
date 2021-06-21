4a89a78a29d58253c6ac67437d54797b930aaa7d
mujin-pro <greedy.hyena.boy@gmail.com>
2021-06-21 14:37:49 +0900

    結合テストコード（途中まで）
    単体テストコードまでの指示だったが、誤って結合テストコードまで書いてしまっていた

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -8,25 +8,60 @@ RSpec.describe 'ユーザー新規登録', type: :system do
   context 'ユーザー新規登録ができるとき' do
     it '正しい情報を入力すればユーザー新規登録ができ、トップページに戻る' do
       #トップページに移動する
+      visit root_path
       #トップページのヘッダーにサインアップページへの遷移ボタンがある（ログアウト状態時）
+      expect(page).to have_content('新規登録')
       #新規登録ボタンを押すとサインアップページへ遷移できる
-      #サインアップページへ移動する
       #ユーザー情報を入力する
+      fill_in('ニックネーム') with: @user.nickname
+      fill_in('メールアドレス') with: @user.email
+      fill_in('パスワード') with: @user.password
+      fill_in('パスワード（確認）') with: @user.password_confirmation
+      fill_in('お名前（全角）') with: @user.family_name
+      fill_in(user[first_name]) with: @user.first_name
       #サインアップボタンを押すと、ユーザーモデルのカウントが1上がる
+      expect{ 
+        find('input[name = "commit"]').click 
+      }.to change { User.count }.by(1)
       #トップページへ遷移したことを確認する
+      expect(current_path).to eq(root_path)
       #ヘッダーにサインアップページへ遷移するボタン/ログインページへ遷移するボタンがなく、ユーザーのニックネーム/ログアウトボタンがある
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+      expect(page).to have_content(@user.nickname)
+      expect(page).to have_content('ログアウト')
     end
 
     it 'ログアウトボタンを押すとログアウトし、トップページに戻る' do
       #トップページに移動する
+      visit root_path
       #トップページのヘッダーにサインアップページへの遷移ボタンがある（ログアウト状態時）
+      expect(page).to have_content('新規登録')
       #新規登録ボタンを押すとサインアップページへ遷移できる
+      
       #サインアップページへ移動する
+
       #ユーザー情報を入力する
+      fill_in('ニックネーム') with: @user.nickname
+      fill_in('メールアドレス') with: @user.email
+      fill_in('パスワード') with: @user.password
+      fill_in('パスワード（確認）') with: @user.password_confirmation
+      fill_in('お名前（全角）') with: @user.family_name
+      fill_in(user[first_name]) with: @user.first_name
+      
       #サインアップボタンを押すと、ユーザーモデルのカウントが1上がる
+      expect{ 
+        find('input[name = "commit"]').click 
+      }.to change { User.count }.by(1)
       #トップページへ遷移したことを確認する
+      expect(current_path).to eq(root_path)
       #ヘッダーにサインアップページへ遷移するボタン/ログインページへ遷移するボタンがなく、ユーザーのニックネーム/ログアウトボタンがある
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+      expect(page).to have_content(@user.nickname)
+      expect(page).to have_content('ログアウト')
       #ログアウトボタンを押すと、トップページに遷移したことを確認する
+
       #ヘッダーにサインアップページへ遷移するボタン/ログインページへ遷移するボタンがあり、ユーザーのニックネーム/ログアウトボタンがない
     end
 
