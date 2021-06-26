7c5acbbde177d365fd8e77d0e0b604202f83286c
mujin-pro <greedy.hyena.boy@gmail.com>
2021-06-27 06:56:19 +0900

    プルダウンの実装（ActiveHash）

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,18 @@
 class Product < ApplicationRecord
   belongs_to :user
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :charge, :source, :day
+
   with_options presence: true do
-    validates :name,                  length: { maxium: 40 }
-    validates :product_description,   length: { maxium: 1000 }
-    validates :product_category_id
-    validates :product_status_id
-    validates :delivery_charge_id
-    validates :delivery_source_id
-    validates :days_up_to_delivery_id
-    validates :price,                 numericality: { in: 300..9999999 }
+    validates :name,                   length: { maxium: 40 }
+    validates :product_description,    length: { maxium: 1000 }
+    validates :product_category_id,    numericality: { other_than: 1 }
+    validates :product_status_id,      numericality: { other_than: 1 }
+    validates :charge_id,              numericality: { other_than: 1 }
+    validates :delivery_source_id,     numericality: { other_than: 1 }
+    validates :days_up_to_delivery_id, numericality: { other_than: 1 }
+    validates :price,                  numericality: { in: 300..9999999 }
   end
 
 end
