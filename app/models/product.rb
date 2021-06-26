f034b13a4a1895929c0a47316737836e6b2a2103
mujin-pro <greedy.hyena.boy@gmail.com>
2021-06-26 18:19:42 +0900

    モデルへのバリデーション設定・ActiveHashのGemインストール

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :name,                  length: { maxium: 40 }
+    validates :product_description,   length: { maxium: 1000 }
+    validates :product_category_id
+    validates :product_status_id
+    validates :delivery_charge_id
+    validates :delivery_source_id
+    validates :days_up_to_delivery_id
+    validates :price,                 numericality: { in: 300..9999999 }
+  end
+
 end
