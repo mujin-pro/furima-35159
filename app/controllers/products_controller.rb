92776d1d1e2e822d998bd1209b37e144b3ad9c2b
mujin-pro <greedy.hyena.boy@gmail.com>
2021-06-30 08:26:05 +0900

    商品の入力・保存機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,29 @@
 class ProductsController < ApplicationController
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product= Product.create(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
   def index
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :image, :name, :product_description,
+      :product_category_id, :product_status_id,
+      :delivery_charge_id, :delivery_source_id,
+      :days_up_to_delivery_id, :price
+    ).merge(user_id: current_user.id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,11 +6,11 @@ class Product < ApplicationRecord
   belongs_to :category, :status, :charge, :source, :day
 
   with_options presence: true do
-    validates :name,                   length: { maxium: 40 }
-    validates :product_description,    length: { maxium: 1000 }
+    validates :name,                   length: { maximum: 40 }
+    validates :product_description,    length: { maximum: 1000 }
     validates :product_category_id,    numericality: { other_than: 1 }
     validates :product_status_id,      numericality: { other_than: 1 }
-    validates :charge_id,              numericality: { other_than: 1 }
+    validates :delivery_charge_id,     numericality: { other_than: 1 }
     validates :delivery_source_id,     numericality: { other_than: 1 }
     validates :days_up_to_delivery_id, numericality: { other_than: 1 }
     validates :price,                  numericality: { in: 300..9999999 }
